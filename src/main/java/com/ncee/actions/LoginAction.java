4969f3642d5ac5dd105383dd82cd59b4a2849e31
jungle0127 <arbor@foxmail.com>
2017-06-07 22:41:47 +0800

    Added logic for login

--- src/main/java/com/ncee/actions/LoginAction.java
+++ src/main/java/com/ncee/actions/LoginAction.java
@@ -1,5 +1,7 @@
 package com.ncee.actions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
@@ -10,31 +12,34 @@ import org.springframework.stereotype.Service;
 import com.opensymphony.xwork2.ActionSupport;
 
 @Namespace("/")
-@Results({
-	@Result(name="success",location="/index.jsp"),
-	@Result(name="fail",location="/login.jsp")
-})
-@Service(value="userAction")
-@Scope(value="prototype")
+@Results({ @Result(name = "success", location = "/WEB-INF/index.jsp"),
+		@Result(name = "error", location = "/login.jsp") })
+// @Service(value="userAction")
+@Scope(value = "prototype")
 public class LoginAction extends ActionSupport {
 	private static final long serialVersionUID = -2860468326563825851L;
-	
-	@Action(value="/login")
-	public String login(){
+	private static final Logger logger = LogManager.getLogger(LoginAction.class);
+
+	@Action(value = "/login")
+	public String login() {
 		return SUCCESS;
 	}
-	
+
 	private String loginName;
 	private String password;
+
 	public String getLoginName() {
 		return loginName;
 	}
+
 	public void setLoginName(String loginName) {
 		this.loginName = loginName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
