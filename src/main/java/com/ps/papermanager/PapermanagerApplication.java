db91d9508164cb7956f877371dc9e1f11f75276c
jungle0127 <arbor@foxmail.com>
2018-01-28 14:02:21 +0800

    added swagger

---
+++ src/main/java/com/ps/papermanager/PapermanagerApplication.java
@@ -0,0 +1,12 @@
+package com.ps.papermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapermanagerApplication.class, args);
+	}
+}
