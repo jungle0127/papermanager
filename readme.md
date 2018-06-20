b0186989e8318c83be79a62676eeaacfc72601d3
jungle0127 <arbor@foxmail.com>
2018-06-20 22:26:11 +0800

    updated UT from dbunit to mockito

--- readme.md
+++ readme.md
@@ -7,5 +7,5 @@ https://github.com/aerogear/aerogear-unifiedpush-server/tree/release-1.1.0/admin
 https://ng-bootstrap.github.io/#/home
 
 #UT
-
+mock
 https://blog.csdn.net/atec2000/article/details/54346242
--- src/test/java/com/ncee/service/UsersServiceTest.java
+++ src/test/java/com/ncee/service/UsersServiceTest.java
@@ -1,8 +1,16 @@
 package com.ncee.service;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.Any;
+import org.mockito.runners.MockitoJUnit44Runner;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,24 +23,39 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.ncee.dao.domain.UsersMapper;
 import com.ncee.dao.model.Users;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration
-@TestExecutionListeners({
-	DependencyInjectionTestExecutionListener.class,
-	DirtiesContextTestExecutionListener.class,
-	TransactionalTestExecutionListener.class,
-	DbUnitTestExecutionListener.class})
-@DbUnitConfiguration(databaseConnection="primaryDatasource")
+//@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
+//@SpringBootTest
+//@ContextConfiguration
+//@TestExecutionListeners({
+//	DependencyInjectionTestExecutionListener.class,
+//	DirtiesContextTestExecutionListener.class,
+//	TransactionalTestExecutionListener.class,
+//	DbUnitTestExecutionListener.class})
+//@DbUnitConfiguration(databaseConnection="primaryDatasource")
 public class UsersServiceTest {
-	@Autowired
+	@InjectMocks
 	private UsersService userService;
+	@Mock
+	private UsersMapper usersMapper;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
 	@Test
-	@DatabaseSetup("classpath:dbunit/users.xml")
+//	@DatabaseSetup("classpath:dbunit/users.xml")
 	public void testFindUser() {
-		Users pojo = this.userService.findUser("ps", "lotus");
+		Users userPojo = new Users();
+		userPojo.setUsername("psarbor");
+		Mockito.when(this.usersMapper.selectByLogin(Mockito.any(Users.class))).thenReturn(userPojo);
+		
+		Users pojo = this.userService.findUser("ps", "lotus");	
 		Assert.assertNotNull(pojo);
+		Assert.assertEquals(pojo, userPojo);
 	}
 }
